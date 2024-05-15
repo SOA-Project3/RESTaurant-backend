7aaa4334c6af31749f473f8031ca02288f30ec71
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-05-14 18:01:25 -0600

    CORS error

--- src/index.js
+++ src/index.js
@@ -12,17 +12,6 @@ app.use(morgan("tiny")); //Log request
 app.use(bodyParser.json());
 
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 // Use the cors middleware
 app.use(cors());
 
