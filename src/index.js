19870986d96f1a14cd1cb77c41531062783b5309
valesskablanco <valesskablanco@estudiantec.cr>
2024-06-10 22:19:58 -0600

    cleaning up menu service

--- src/index.js
+++ src/index.js
@@ -15,38 +15,8 @@ app.use(bodyParser.json());
 // Use the cors middleware
 app.use(cors());
 
-const booking = require("./controllers/Booking");
-router.get("/allScheduleSlots", booking.allScheduleSlots); 
-router.get("/availableScheduleSlots", booking.availableScheduleSlots); 
-router.get("/bookedScheduleSlots", booking.bookedScheduleSlots); 
-router.get("/userScheduleSlots", booking.userScheduleSlots); 
-router.put("/bookScheduleSlot", booking.bookScheduleSlot); 
-router.put("/cancelScheduleSlot", booking.cancelScheduleSlot); 
-router.put("/updateScheduleSlotQuantity", booking.updateScheduleSlotQuantity); 
-router.delete("/deleteScheduleSlot", booking.deleteScheduleSlot); 
-router.post("/createScheduleSlot", booking.createScheduleSlot); 
-
-
 const recommendationController = require("./controllers/Recommendations");
 router.get("/recommendations", recommendationController.getRecommendation); 
-
-const hourSuggestion = require("./controllers/HourSuggestions");
-router.get("/suggestions", hourSuggestion.getHourSuggestion); 
-
-const submitFeedback = require("./controllers/ChatBot");
-router.post("/sendFeedback", submitFeedback.submitFeedback); 
-
-const getFeedback = require("./controllers/ChatBot");
-router.get("/getFeedback", getFeedback.getFeedback); 
-
-const auth = require("./controllers/Authenticator");
-router.post("/login", auth.login); 
-router.post("/register", auth.register); 
-router.get("/getUserbyId", auth.userById); 
-router.get("/deleteUser", auth.deleteUser); 
-router.get("/resetPassword", auth.resetPassword); 
-router.post("/updatePassword", auth.updatePassword); 
-
 
 
 app.get('/menu', (req, res) => {
